04d83031f2ba04d2bff0d3b3f7031948432b2348
Jacob Whisler <jwhisler521@gmail.com>
2019-12-02 19:37:03 -0500

    added functionality to start button to make sure the order of the questions is random every time

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 var startBtn = document.querySelector('#start-btn')
 var questContainEl = document.querySelector('#question-container')
 
-startBtn.addEventListener('click', startGame)
+var shuffeledQuestions; 
+var currentQuestionsInsex;
 
+
+startBtn.addEventListener('click', startGame)
 
 function startGame() {
     startBtn.classList.add('hide')
+    shuffeledQuestions = questions.sort(function() {Math.random() -.5})
+    currentQuestionsInsex = 0;
     questContainEl.classList.remove('hide')
     nextQuestion()
 }
