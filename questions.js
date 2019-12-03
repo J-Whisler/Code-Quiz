3d89c5d76c9ee57df65e397544f5c9bbe540f8df
Jacob Whisler <jwhisler521@gmail.com>
2019-12-02 19:27:52 -0500

    added 5 questions to questions.js

--- questions.js
+++ questions.js
@@ -1,2 +1,55 @@
+var questions = [
+    {
+        question: 'What does DOM stand for?',
+        answers: [
+            {text: 'Document Object Model', correct: true},
+            {text: 'Desktop Operation Manager', correct: false},
+            {text: 'Document Oreientanted Machine', correct: false},
+            {text: 'Definitive Object Module', correct: false}
+        ]
+    },
+
+    {
+        question: 'Where in your index.html should you put the link to your javascript file?',
+        answers: [
+            {text: 'At the bottom of the <body>', correct: true},
+            {text: 'At the top of the <body>', correct: false},
+            {text: 'Below the closing </body> tag', correct: false},
+            {text: 'Above the opening <head> tag'}
+        ]
+    },
+
+    {
+            question: 'What is a boolean?',
+            answers: [
+                {text: 'True or False', correct: true},
+                {text: 'A number', correct: false},
+                {text: 'A set of characters or words between quotation marks', correct: false},
+                {text: 'A ghost with one leg', correct: false}
+            ]
+    },
+
+    {
+        question: 'How do you comment out a single line of code in Javascript?',
+        answers: [
+            {text: '// this is a comment', correct: true},
+            {text: '/ this is a commnet', correct: false},
+            {text: '<!-- this is a comment -->', correct: false},
+            {text: '... this is a comment ...', correct: false}
+        ]
+    },
+
+    {
+        question: 'How do you declare a variable (ES5)?',
+        anwers: [
+            {text: var myVariable = 'This is my variable', correct: true},
+            {text: myVariable = 'This is my variable', correct: false},
+            {text: declare myVariable = 'This is my variable', correct: false},
+            {text: variable myVariable = 'This is my variable', correct: false}
+        ]
+    }
+
+]
+
 
   
